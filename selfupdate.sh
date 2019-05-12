3486641fc60e80814409eff05f13ded6ca7e36ff
Valery Klimin <vdos63@gmail.com>
2019-05-12 21:09:39 +0300

    Update selfupdate.sh

--- selfupdate.sh
+++ selfupdate.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/TheOrangeCat/kot-pkg/master/kot-pkg -O $1/kot-pkg
+wget -q https://raw.githubusercontent.com/TheOrangeCat/kot-pkg/master/kot-pkg -O $1/kot-pkg
