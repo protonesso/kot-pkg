e0e31e8534b437911d75e156463e98580a0076bb
Valery Klimin <vdos63@gmail.com>
2019-05-18 15:46:07 +0300

    Update selfupdate.sh

--- subroutines/selfupdate.sh
+++ subroutines/selfupdate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "selfupdate.sh v0.02"
 echo "Updating kot-pkg"
-wget -q https://raw.githubusercontent.com/TheOrangeCat/kot-pkg/master/kot-pkg -O $1/kot-pkg
+wget  https://raw.githubusercontent.com/TheOrangeCat/kot-pkg/master/kot-pkg -O $1/kot-pkg
