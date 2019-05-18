246cf19d915684e3506e53bd7419e9af2db6307d
Valery Klimin <vdos63@gmail.com>
2019-05-18 11:17:56 +0300

    Update installpkg.sh

--- repo/balena-engine/installpkg.sh
+++ repo/balena-engine/installpkg.sh
@@ -1,3 +1,8 @@
-echo "test in prod"
-echo "arch is $hosttype"
-echo "install folder is $1"
+pkgver=18.9.5
+wget -q https://github.com/balena-os/balena-engine/archive/v$pkgver.tar.gz -O $1/../pkgtemp/balena-engine.tar.gz
+mkdir $1/../pkgtemp/balena-engine
+tar -xvzf $1/../pkgtemp/balena-engine.tar.gz --one-top-level=$1/../pkgtemp/balena-engine/
+cd $1/../pkgtemp/balena-engine
+make && make install
+cd $pwd
+
